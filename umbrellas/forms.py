8ae10d6931e6dac518c81abfb5f8dc938f2d1e0b
Tausei722 <kusaki.kei825@gmail.com>
2025-04-29 00:34:36 +0900

    サインアップの微修正　パスワードの確認

--- umbrellas/forms.py
+++ umbrellas/forms.py
@@ -9,6 +9,7 @@ class CustomForm(forms.ModelForm):
     username = forms.CharField(label='名前',widget=forms.TextInput(attrs={'class': 'border border-[#808080] rounded-full px-2 bg-white w-full h-[50px]'}))
     email = forms.EmailField(label='メール',widget=forms.TextInput(attrs={'class': 'border border-[#808080] rounded-full px-2 bg-white w-full h-[50px]'}))
     password = forms.CharField(label='パスワード',widget=forms.TextInput(attrs={'class': 'border border-[#808080] rounded-full px-2 bg-white w-full h-[50px]'}))
+    password_confirm = forms.CharField(label='パスワード確認',widget=forms.TextInput(attrs={'class': 'border border-[#808080] rounded-full px-2 bg-white w-full h-[50px]'}))
     faculty = forms.ChoiceField(choices=STATUS_FACULTY, label='学部',widget=forms.Select(attrs={'class': 'border border-[#808080] rounded-full px-2 bg-white w-full h-[50px] px-4'}))
     grade = forms.ChoiceField(choices=STATUS_GRADE, label='学年',widget=forms.Select(attrs={'class': 'border border-[#808080] rounded-full px-2 bg-white w-full h-[50px] px-4'}))
     sex = forms.ChoiceField(choices=STATUS_SEX, label='性別',widget=forms.Select(attrs={'class': 'border border-[#808080] rounded-full px-2 bg-white w-full h-[50px] px-4'}))
@@ -21,6 +22,11 @@ class CustomForm(forms.ModelForm):
     # パスワードのバリデーション機能(パスワードだけバリデーションを分ける)
     def clean_password(self):
         password = self.cleaned_data.get('password')
+        password_confirm = self.cleaned_data.get('password_confirm')
+
+        if password != password_confirm:
+            raise forms.ValidationError('パスワードが確認と違います')
+
         try:
             validate_password(password)
         except ValidationError as e:
