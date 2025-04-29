14e147e55e5cc4af99eca059739f1a44c67f79d2
mj836 <kisuk2448@icloud.com>
2025-04-30 06:04:39 +0900

    メニュ―作った

--- static/js/menu.js
+++ static/js/menu.js
@@ -5,13 +5,13 @@ document.addEventListener("DOMContentLoaded", function () {
     const hamburgerIcon = document.querySelector(".hamburger-icon");
     const closeButton = document.querySelector(".close-btn");
   
-    // メニュー開閉
+    // メニュー開閉 //
     hamburgerIcon.addEventListener("click", function () {
       menu.classList.toggle("active");
       menuWrapper.classList.toggle("active");
     });
   
-    // 閉じるボタンでメニューを閉じる
+    // 閉じるボタンでメニューを閉じる //
     closeButton.addEventListener("click", function () {
       menu.classList.remove("active");
       menuWrapper.classList.remove("active");
