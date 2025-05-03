a553ccf672945fc777e2f5b6b880d4c5b271004c
Tausei722 <kusaki.kei825@gmail.com>
2025-05-03 22:51:58 +0900

    URL謎追加

--- rental_umbrella/settings.py
+++ rental_umbrella/settings.py
@@ -138,7 +138,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_URL = '/umbrellas/login/'
+LOGIN_URL = '/login/'
 
 # 自動ログインのための設定
 SESSION_ENGINE = "django.contrib.sessions.backends.db"
