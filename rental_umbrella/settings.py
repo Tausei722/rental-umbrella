1b25769bf81d6836add301f0b52ee33d8715fbb8
Tausei722 <kusaki.kei825@gmail.com>
2025-05-04 16:01:59 +0900

    sdgfdt

--- rental_umbrella/settings.py
+++ rental_umbrella/settings.py
@@ -139,6 +139,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = None
 
 # 自動ログインのための設定
 SESSION_ENGINE = "django.contrib.sessions.backends.db"
