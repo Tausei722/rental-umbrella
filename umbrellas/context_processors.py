698106372cb4100378eaac13848037ea7d6dd9b5
Tausei722 <kusaki.kei825@gmail.com>
2025-05-05 18:07:52 +0900

    かさの所有DBに入れる

--- umbrellas/context_processors.py
+++ umbrellas/context_processors.py
@@ -3,5 +3,7 @@ from .models import Umbrellas
 def borrowed_umbrella(request):
     if request.user.is_authenticated:
         umbrella = Umbrellas.objects.filter(borrower=request.user).first()
+        if umbrella:
+            request.user.borrowed_umbrella = umbrella
         return {"borrowed_umbrella": umbrella}
     return {"borrowed_umbrella": None}
